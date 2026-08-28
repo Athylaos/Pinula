21ea1bbdaddcae4aad912bc9ca0c8c43090475ff
David Hykyš <davidhykys88@gmail.com>
2026-08-28 09:09:40 +0200

    DB migrations

---
+++ Pinula.API/Migrations/20260827133719_FixMealPlanIngredientCascade.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pinula.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMealPlanIngredientCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_meal_plan_ingredients_meal_plans_meal_plan_id",
+                table: "meal_plan_ingredients");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_meal_plan_ingredients_meal_plans_meal_plan_id",
+                table: "meal_plan_ingredients",
+                column: "meal_plan_id",
+                principalTable: "meal_plans",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_meal_plan_ingredients_meal_plans_meal_plan_id",
+                table: "meal_plan_ingredients");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_meal_plan_ingredients_meal_plans_meal_plan_id",
+                table: "meal_plan_ingredients",
+                column: "meal_plan_id",
+                principalTable: "meal_plans",
+                principalColumn: "id");
+        }
+    }
+}
